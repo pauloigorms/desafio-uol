691ef2a6716759e83f8a4c884c51c6c66df5c4f1
Paulo Moraes <pauloigormoraes@gmail.com>
2021-01-03 17:58:48 -0400

    initilize docker and docker compose

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -10,7 +10,8 @@ import { AppService } from './app.service'
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/uuol'),
+    MongooseModule.forRoot('mongodb+srv://uuol:0lVYbsOblztb5SQ8@catalog.j1eua.mongodb.net/uuol?retryWrites=true&w=majority'),
+    //MongooseModule.forRoot('mongodb://localhost:27017/uuol'),
     GraphQLModule.forRoot({    
         include: [ProductsModule],
         autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
